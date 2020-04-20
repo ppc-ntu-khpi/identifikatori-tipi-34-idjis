71298b77dd6ccdda508740d22908ad1a0698698d
idjis <63704562+idjis@users.noreply.github.com>
2020-04-20 11:59:45 +0300

    Update Exercise.java

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -10,7 +10,7 @@ public class Exercise {
 	 * @return true - the program will return if the driven data will meet the specified condition
 	 * @return false - will return if the data does not meet the specified condition
 	 */
-    public boolean isPalindrome(String original){
+    public boolean Calculate(String original){
         int i = original.length()-1;
         int j=0;
         while(i > j) {
