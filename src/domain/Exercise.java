2fb1aea2d45fc3b8bbadd216d3a9530b477996e0
idjis <63704562+idjis@users.noreply.github.com>
2020-04-20 11:42:22 +0300

    Update

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,25 @@
 package domain;
-
+/**
+ * Містить реалізацію методу
+ * @author IT-Service
+ */
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+	/**
+	 * Метод що містить реалізацію заданої задачі
+	 * @param original
+	 * @return true - поверне программа якщо ведені дані будуть відповідати заданій умові
+	 * @return false - поверне якщо дані не будуть відповідати заданій умові
+	 */
+    public boolean isPalindrome(String original){
+        int i = original.length()-1;
+        int j=0;
+        while(i > j) {
+            if(original.charAt(i) != original.charAt(j)) {
+                return false;
+            }
+            i--;
+            j++;
+        }
+        return true;
     }
 }
--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -1,12 +1,28 @@
 package test;
 
+import java.util.Scanner;
 import domain.Exercise;
-
+/**
+ * Містить реалізацію виводу результатів
+ * @author IT-Service
+ */
 public class TestResult {
-
-    public static void main(String[] args) {
-
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+	private static Scanner input;
+	/**
+	 * Метод який містить вивід результатів методу isPalindrome
+	 * @param args - містить аргументи що надаються
+	 */
+	public static void main(String [] args){
+		input = new Scanner(System.in);
+		System.out.print("Input name: ");
+		String text = input.nextLine();
+		text = text.replaceAll("[^A-Za-zА-Яа-я0-9]", "");
+		Exercise res = new Exercise();
+		if(res.isPalindrome(text)){
+		    System.out.println(text + " - Palindrome");
+		} else {
+		    System.out.println(text + " - Not a Palindrome");
+		}
+		
     }
 }
